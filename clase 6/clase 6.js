92732a120cd240a7ffb8b2ea71091349c6140002
Diego Mierez <dmierez@gmail.com>
2020-02-29 18:53:39 -0300

    3era clase, hasta arrays

--- clase 6/clase 6.js
+++ clase 6/clase 6.js
@@ -26,13 +26,47 @@
 // }
 
 
-var x = 4
-var y = '4'
+// var x = 4
+// var y = '4'
+
+// var diego = {
+//     nombre: 'diego'
+// }
+
+// var otroDiego = {
+//     ...diego
+// }
+
+
+// 
 
 var diego = {
-    nombre: 'diego'
+    nombre: 'diego',
+    apellido: 'mierez',
+    edad: 34,
+    peso: 85
 }
 
-var otroDiego = {
-    ...diego
+console.log(`al inicio del año ${diego.nombre} pesa ${diego.peso}kg`)
+
+const incremento = 0.3
+
+const aumento = persona => persona.peso += incremento
+const bajar = persona => persona.peso -= incremento
+const comer = () => Math.random() < 0.3
+const deporte = () => Math.random() < 0.4
+
+const meta = diego.peso - 3
+var dias = 0
+
+while (diego.peso > meta) {
+    if (comer()) {
+        aumento(diego)
+    }
+    if (deporte()) {
+        bajar(diego)
+    }
+    dias +=1
 }
+
+console.log(`pasaron ${dias} días hasta que ${diego.nombre} bajó 3kg`)
