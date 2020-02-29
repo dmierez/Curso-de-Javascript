b44fab51dace3b77a4260a9a86c97cdec443092e
Diego Mierez <dmierez@gmail.com>
2020-02-28 23:09:10 -0300

    2da parte del curso de fundamentos de js

---
+++ clase 5/clase 5.js
@@ -0,0 +1,8 @@
+var nombre = 'Diego'
+
+function imprimir(d) {
+    d = d.toUpperCase()
+    console.log(d)
+}
+
+imprimir(nombre)
---
+++ clase 6/clase 6.js
@@ -0,0 +1,38 @@
+// var diego = {
+//     nombre: 'diego',
+//     apellido: 'mierez',
+//     edad: 34
+// }
+
+// var viejo = {
+//     nombre: 'gerardo',
+//     apellido: 'mierez',
+//     edad: 59
+// }
+
+// function imprimir(persona) {
+//     var { nombre, edad } = persona
+//     console.log('Hola me llamo ' + nombre.toUpperCase() + ' y tengo ' + edad + ' años')
+// }
+
+// imprimir(diego)
+// imprimir(viejo)
+
+// function cumple(persona) {
+//     return {
+//         ...persona,
+//         edad: persona.edad +1
+//     }
+// }
+
+
+var x = 4
+var y = '4'
+
+var diego = {
+    nombre: 'diego'
+}
+
+var otroDiego = {
+    ...diego
+}
